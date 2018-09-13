e4c1ff23615e6a4b9c7a06fc4cab278ee126c22c
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-09-13 22:30:47 +0200

    Added code for inserting data into the database

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using Model;
 
 namespace Lecture1
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+			using (var db = new MovieContext())
+			{
+
+				Movie m = new Movie
+				{
+					Title = "No country for old men",
+					Actors = new System.Collections.Generic.List<Actor> {
+						new Actor{Name = "Tommy Lee"},
+						new Actor{Name = "Xavier Berdem"}
+					}
+				};
+				db.Movies.Add(m);
+				db.SaveChanges();
+			}
+
+      Console.WriteLine("Hello World!");
     }
+  }
 }
