563937081a3ecf46b565f5ff6301290d55e062f0
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-09-20 23:01:28 +0200

    Added different kind of query's

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -52,15 +52,80 @@ namespace Lecture1
 				// 												select m;
 
 				// Projetion with ordering
-				var projected_movies = 	from m in db.Movies
-																where m.Release > new DateTime(2000, 1, 1)
-																orderby m.Release descending
-																select m;
+				// var projected_movies = 	from m in db.Movies
+				// 												where m.Release > new DateTime(2000, 1, 1)
+				// 												orderby m.Release descending
+				// 												select m;
 
-				Console.WriteLine("Movie title | release");
-				foreach (var movie in projected_movies)
-				{
-					Console.WriteLine("- {0} | {1}", movie.Title, movie.Release);
+				// Grouping
+				// var projected_movies = 	from a in db.Actors
+				// 												group a by a.Gender into genderGroup
+				// 												select genderGroup;
+
+				// Aggregation
+				// var result = 	from movie in db.Movies
+				// 							from actor in db.Actors
+				// 								where actor.MovieId == movie.Id
+				// 								group actor by actor.Gender into GenderGrp
+				// 							select Tuple.Create (
+				// 								GenderGrp.Key,
+				// 								GenderGrp.Count()
+				// 							);
+
+				// Joining
+				// var projected_movies = 	from movie in db.Movies
+				// 												from actor in db.Actors
+				// 												where movie.Id == actor.Id
+				// 												select new {
+				// 													Title = movie.Title,
+				// 													actorName = actor.Name
+				// 												};
+
+				// Subquery and aggregation
+				var projected_movies = 	from movie in db.Movies
+																let actors_of_movie = (
+																	from actor in db.Actors
+																	where actor.MovieId == movie.Id
+																	select actor
+																)
+																where actors_of_movie.Count() < 3
+																select new {
+																	Title = movie.Title,
+																	ActorsCount = actors_of_movie.Count()
+																};
+
+
+				// Write movies
+				// Console.WriteLine("Movie title | release");
+				// foreach (var movie in projected_movies)
+				// {
+				// 	Console.WriteLine("- {0} | {1}", movie.Title, movie.Release);
+				// }
+
+				// Write actors (grouping)
+				// foreach (var movie in projected_movies){
+				// 		Console.WriteLine("+ {0} ", movie.Key);
+				// 		foreach (var actor in movie){
+				// 				Console.WriteLine("-- {0} ", actor.Name);
+				// 		}
+				// }
+
+				// Write actors (aggregation)
+				// Console.WriteLine("Gender | Number of actors");
+				// foreach (var item in result){
+				// 					Console.WriteLine("{0} | {1}", item.Item1, item.Item2);
+				// }
+
+				// Write joining
+				// Console.WriteLine("Movie | Actor");
+				// foreach (var item in projected_movies){
+				// 					Console.WriteLine("{0} | {1}", item.Title, item.actorName);
+				// }
+
+				// Write subquery and aggregation
+				Console.WriteLine("Movie | Actor count");
+				foreach (var item in projected_movies){
+									Console.WriteLine("{0} | {1}", item.Title, item.ActorsCount);
 				}
 			}
 		}
