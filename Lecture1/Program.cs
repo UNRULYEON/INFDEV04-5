996641b572b43a8f2f08e94eebeab623e82896fd
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2018-09-20 22:41:15 +0200

    Added data and first projection

--- Lecture1/Program.cs
+++ Lecture1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Model;
 
 namespace Lecture1
@@ -7,28 +8,56 @@ namespace Lecture1
   {
     static void Main(string[] args)
     {
+			DataInsertion();
+			Projection();
+			Join();
+    }
+
+		static void DataInsertion()
+		{
+			using (var db = new MovieContext())
+			{
+				// Movie m = new Movie{
+				// 	Title = "Divorce Italian Style",
+				// 	Release = DateTime.Now,
+				// 	Actors = new System.Collections.Generic.List<Actor> {
+				// 		new Actor {
+				// 			Name = "Marcello Mastroianni",
+				// 			Birth = new DateTime(1988, 8, 29),
+				// 			Gender = "Male"
+				// 		},
+				// 		new Actor {
+				// 			Name = "Daniela Rocca",
+				// 			Birth = new DateTime(1986, 5, 1),
+				// 			Gender = "Female"
+				// 		}
+				// 	}
+				// };
+
+				// db.Add(m);
+				// db.SaveChanges();
+			}
+		}
+
+		static void Projection()
+		{
+			using (var db = new MovieContext())
+			{
+				var projected_movies = from m in db.Movies select m;
+				Console.WriteLine("Movie title | release");
+				foreach (var movie in projected_movies)
+				{
+					Console.WriteLine("- {0} | {1}", movie.Title, movie.Release);
+				}
+			}
+		}
+
+		static void Join()
+		{
 			using (var db = new MovieContext())
 			{
 
-				// Movie m = new Movie
-				// {
-				// 	Title = "No country for old men",
-				// 	Actors = new System.Collections.Generic.List<Actor> {
-				// 		new Actor{Name = "Tommy Lee"},
-				// 		new Actor{Name = "Xavier Berdem"}
-				// 	}
-				// };
-				// db.Movies.Add(m);
-				// db.SaveChanges();
-
-				// Movie foundMovie = db.Movies.Find(1);
-				// Console.WriteLine("Found movie with title" + foundMovie.Title);
-				// foundMovie.Title = "White cats, Black cats...";
-				// db.SaveChanges();
-				// Console.WriteLine("Title changed");
 			}
-
-      Console.WriteLine("Hello World!");
-    }
+		}
   }
 }
